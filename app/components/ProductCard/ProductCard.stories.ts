8c40c510d1dc2c0e718129a5ecd721104215720b
thirdlf03 <thirdlf03@gmail.com>
2025-03-26 01:58:47 +0900

    feat: add icon

--- app/components/ProductCard/ProductCard.stories.ts
+++ app/components/ProductCard/ProductCard.stories.ts
@@ -14,14 +14,14 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Hello: Story = {
+export const NekoBot: Story = {
   args: {
     headerImage: "https://pbs.twimg.com/profile_banners/1846395762277826560/1737992837/1500x500",
     title: "Neko Bot",
-    stacks: ["go", "jenkins"],
+    stacks: ["Go", "Jenkins"],
     description: "ねこbotは団体discordで運用しているbotです。\n団体内での情報共有や、AIによる質問返答、グループ管理の役割を担っています。",
     stars: 1,
-    forks: 142,
+    commits: 142,
     pullRequests: 75,
   }
 }
