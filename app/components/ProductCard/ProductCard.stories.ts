b4d8b3f0f0247e4af092961fee384fbe03f58602
thirdlf03 <thirdlf03@gmail.com>
2025-03-31 16:20:16 +0900

    feat: add buttons to product card

--- app/components/ProductCard/ProductCard.stories.ts
+++ app/components/ProductCard/ProductCard.stories.ts
@@ -23,5 +23,34 @@ export const NekoBot: Story = {
     stars: 1,
     commits: 142,
     pullRequests: 75,
+    linkedButtons: [
+      {
+        url: 'https://github.com/naoido/neko-bot',
+        label: 'Github',
+      },
+    ]
   }
 }
+
+export const Website: Story = {
+  args: {
+    headerImage: "https://pbs.twimg.com/profile_banners/1846395762277826560/1737992837/1500x500",
+    title: "団体公式サイト",
+    stacks: ["Cloudflare", "React", "Cloudflareworkers"],
+    description: "団体公式のウェブサイトです。Reactで構成されており、Cloudfalre Workers使用し記事の更新などを行い、Cloudflare Pagesにデプロイしています。",
+    stars: 2,
+    commits: 86,
+    pullRequests: 54,
+    linkedButtons: [
+      {
+        url: 'https://github.com/object-t/object-t-website',
+        label: 'Github',
+      },
+      {
+        url: "https://www.figma.com/design/YWBtX9qhd0QKOTY4a2SEWx",
+        label: "Figma",
+      }
+    ]
+  }
+}
+
